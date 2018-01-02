627fb25f42ed1d34a653b4f43147817f9b58328b
Elvis Pranskevichus <elvis@magic.io>
2018-01-02 12:58:35 -0500

    Add better systemd support.  Factor out common code.

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,16 +1,18 @@
-#!/sbin/openrc-run
-# Copyright 1999-2017 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: $
-
-extra_commands="debug enter shell upgrade"
+# Common docker-rc functions.
 
 docker_bin=${docker_bin:-/usr/bin/docker}
 
+if [ x$RC_GOT_FUNCTIONS = xyes -o -n "$(command -v ebegin 2>/dev/null)" ]; then
+    :
 
-depend() {
-	need net docker
-}
+# Then check for the presence of functions.sh
+elif [ -f /lib/gentoo/functions.sh ]; then
+    . /lib/gentoo/functions.sh
+
+else
+    echo "/lib/gentoo/functions.sh not found. Exiting"
+    exit 1
+fi
 
 
 _docker_opts() {
@@ -24,29 +26,29 @@ _docker_opts() {
 }
 
 
-_init() {
+_docker_init() {
 	if [ ! -d /run/dockerized ]; then
 		mkdir /run/dockerized
 	fi
 }
 
 
-_cid() {
-	if [ -e "/run/dockerized/${SVCNAME}" ]; then
-		cat "/run/dockerized/${SVCNAME}"
+_docker_cid() {
+	if [ -e "/run/dockerized/${RC_CONTAINER}" ]; then
+		cat "/run/dockerized/${RC_CONTAINER}"
 	fi
 }
 
 
-_checkrunning() {
-	local cid=$(_cid)
+_docker_check_running() {
+	local cid=$(_docker_cid)
 
 	if [ -n "${cid}" ]; then
 		if "${docker_bin}" ps | grep "${cid}" >/dev/null; then
 			ewarn ""
-			ewarn "Docker reports ${SVCNAME} as running."
+			ewarn "Docker reports ${RC_CONTAINER} as running."
 			ewarn "Please shut it down using"
-			ewarn "    docker stop ${SVCNAME} && docker rm ${SVCNAME}"
+			ewarn "    docker stop ${RC_CONTAINER} && docker rm ${RC_CONTAINER}"
 
 			return 1
 		fi
@@ -90,16 +92,16 @@ _docker_run() {
 		extraopts="${extraopts} --privileged"
 	fi
 
-	echo "${docker_bin}" run --name="${SVCNAME}" $opts \
+	echo "${docker_bin}" run --name="${RC_CONTAINER}" $opts \
 			$(_docker_opts --env env) \
 			$(_docker_opts -p ports) \
 			$(_docker_opts -v volumes) \
 			${extraopts} \
 			"${docker_image}" \
 			$cmd \
-	| logger -i -t "/etc/init.d/${SVCNAME}" -p daemon.info
+	| logger -i -t "/etc/init.d/${RC_CONTAINER}" -p daemon.info
 
-	docker_stderr=$("${docker_bin}" run --name="${SVCNAME}" $opts \
+	docker_stderr=$("${docker_bin}" run --name="${RC_CONTAINER}" $opts \
 			 $(_docker_opts --env env) \
 			 $(_docker_opts -p ports) \
 			 $(_docker_opts -v volumes) \
@@ -110,10 +112,10 @@ _docker_run() {
 	status=$?
 
 	if [ "$status" -ne "0" ]; then
-		echo $docker_stderr | logger -is -t "/etc/init.d/${SVCNAME}" -p daemon.err
+		echo $docker_stderr | logger -is -t "/etc/init.d/${RC_CONTAINER}" -p daemon.err
 
-		"${docker_bin}" stop "${SVCNAME}" 2>/dev/null
-		"${docker_bin}" rm "${SVCNAME}" 2>/dev/null
+		"${docker_bin}" stop "${RC_CONTAINER}" 2>/dev/null
+		"${docker_bin}" rm "${RC_CONTAINER}" 2>/dev/null
 
 		return $status
 	fi
@@ -128,14 +130,14 @@ _docker_run() {
 		fi
 
 		for net in ${docker_networks}; do
-			docker network connect ${net_extraopts} "${net}" "${SVCNAME}"
+			docker network connect ${net_extraopts} "${net}" "${RC_CONTAINER}"
 		done
 	fi
 
-	cid=$(docker inspect --format {{.Id}} ${SVCNAME})
+	cid=$(docker inspect --format {{.Id}} ${RC_CONTAINER})
 
 	if [ -z "${cid}" ]; then
-		eerror "Container seems to have died.  Check logs for details."
+		eerror "RC_CONTAINER seems to have died.  Check logs for details."
 		return 1
 	fi
 
@@ -143,72 +145,38 @@ _docker_run() {
 }
 
 
-start() {
-	_init && _checkrunning || return 1
+_docker_enter() {
+    local cid=$(_docker_cid) pid
 
-	ebegin "Starting dockerized ${SVCNAME}"
+    if [ -n "${cid}" ]; then
+        pid=$(docker inspect --format {{.State.Pid}} "${cid}")
 
-	_docker_run "-i -d" "${docker_command}" > "/run/dockerized/${SVCNAME}"
+        if [ -z "${pid}" ]; then
+            ewarn ""
+            ewarn "${RC_CONTAINER} does not seem to be running."
+            ewarn ""
+            return 1
+        fi
 
-	eend $?
+        /usr/bin/nsenter --mount --uts --ipc --net --pid \
+            --target $pid -- /bin/bash -l
+    else
+        ewarn ""
+        ewarn "${RC_CONTAINER} does not seem to be running."
+        ewarn ""
+        return 1
+    fi
 }
 
 
-debug() {
-	_init && _checkrunning || return 1
-
-	_docker_run "-i -t --rm"
-}
-
-
-shell() {
-	_init && _checkrunning || return 1
-
-	_docker_run "-i -t --rm" "/bin/bash -l"
-}
-
-
-upgrade() {
-	_init && _checkrunning || return 1
-
-	_docker_run "-i -t --rm" "upgrade"
-}
-
-
-enter() {
-	local cid=$(_cid) pid
-
-	if [ -n "${cid}" ]; then
-		pid=$(docker inspect --format {{.State.Pid}} "${cid}")
-
-		if [ -z "${pid}" ]; then
-			ewarn ""
-			ewarn "${SVCNAME} does not seem to be running."
-			ewarn ""
-			return 1
-		fi
-
-		/usr/bin/nsenter --mount --uts --ipc --net --pid \
-						 --target $pid -- /bin/bash -l
-	else
-		ewarn ""
-		ewarn "${SVCNAME} does not seem to be running."
-		ewarn ""
-		return 1
-	fi
-}
-
-
-stop() {
-	ebegin "Stopping dockerized ${SVCNAME}"
-
-	local cid=$(_cid)
+_docker_stop() {
+	local cid=$(_docker_cid)
 
 	if [ -n "${cid}" ]; then
 		"${docker_bin}" stop ${cid} >/dev/null && docker rm ${cid} >/dev/null
 	else
 		ewarn ""
-		ewarn "${SVCNAME} does not seem to be running."
+		ewarn "${RC_CONTAINER} does not seem to be running."
 		ewarn ""
 		return 1
 	fi
